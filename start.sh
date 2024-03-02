cac60262bea47cb63a528b8013570c733853fa6a
Vinijs <vini-spider-6@hotmail.com>
2024-03-02 00:20:03 -0300

    Aula 23 - 01/03/2024

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 $(cat worker.env)
 docker run -d -e AccessKey -e SecretAccessKey -e SegundosTempoEspera -e DefaultConnection \
--e ElasticSearchLogs -e MongoDbLogs --name servico-notas tornese/servico-notas
+-e ElasticSearchLogs -e MongoDbLogs --name servico-notas tornese/servico-notas:latest
